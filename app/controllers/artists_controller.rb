66a6986ca6ea25b39dd4cdddac5e7b3c997116fd
Latricia <tricianickelberry@gmail.com>
2020-07-15 13:25:29 -0700

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,18 @@
 class ArtistsController < ApplicationController
+
+
+def new
+end
+
+def create
+    @artist = Artist.new
+    @artist.name = params[:name]
+    @artist.bio = params[:bio]
+    @artist.save
+    redirect_to artist_path(@artist)
+end
+
+def show
+    
+end
 end
