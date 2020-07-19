39b15e2fbd42bdc80c17b36da68f59d8ec940c6b
Latricia <tricianickelberry@gmail.com>
2020-07-19 09:01:37 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200715182204) do
+ActiveRecord::Schema.define(version: 20200719154611) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20200715182204) do
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string   "string"
+    t.string   "name"
     t.integer  "artist_id"
     t.integer  "genre_id"
     t.datetime "created_at", null: false
